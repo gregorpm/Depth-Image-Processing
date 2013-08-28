41408b70d0fbd77fc001c89cb6d79f83d7fd6406
Greg Meyer <gregorpm@gmail.com>
2013-08-28 16:54:22 -0500

    Updated FindDIP.cmake

--- cmake/modules/FindDIP.cmake
+++ cmake/modules/FindDIP.cmake
@@ -2,18 +2,14 @@
 # Find Depth-Image-Processing (DIP)
 #
 # DIP_FOUND - True if DIP was found.
-# DIP_INCLUDE_DIRS - Directories containing the DIP include files.
+# DIP_INCLUDE_DIR - Directory containing the DIP include files.
 # DIP_LIBRARIES - Libraries need to use DIP.
 
 find_path(DIP_INCLUDE_DIR dip
           HINTS /usr/local/include/
-          PATHS "$ENV{PROGRAMFILES}/dip/include"
+                "$ENV{PROGRAMFILES}/dip/include"
                 "$ENV{PROGRAMW6432}/dip/include"
           )
-
-set(DIP_INCLUDE_DIRS
-    ${DIP_INCLUDE_DIR}
-    )
 
 set(DIP_MODULES
     cameras
@@ -33,6 +29,8 @@ foreach(MODULE ${DIP_MODULES})
   find_library(DIP_${MODULE}_LIBRARY
                NAMES ${MODULE}
                PATHS /usr/local/lib/
+                     "$ENV{PROGRAMFILES}/dip/lib"
+                     "$ENV{PROGRAMW6432}/dip/lib"
                )
 
   set(DIP_LIBRARIES
